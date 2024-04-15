68e34f69f874d5a11c8915ba637fca92d50550d3
ivansirianni <108744343+ivansirianni@users.noreply.github.com>
2024-04-15 13:18:57 -0300

    primer testeo

---
+++ js/busqueda.js
@@ -0,0 +1,134 @@
+//CONSTRUCTOR DE OBJETOS, FORMA TRADICIONAL
+class Cursos{
+    constructor(nombre, dia,docente, lugar){        
+        this.nombre = nombre,
+        this.dia = dia,        
+        this.docente = docente
+        this.lugar = lugar
+    }//metodo
+   
+}
+
+//CURSOS
+//Lunes
+const bioetica = new Cursos("Bioetica del envejecimiento", "Lunes 9hs", "Elvio Galati", "Sarmiento 784. PISO 1 AULA 2")
+const desarrolloDibujo = new Cursos("El desarrollo del dibujo", "Lunes 10:30 hshs", "Laura Capdevila", "Sarmiento 784. PISO 2 AULA 5")
+const emociones = new Cursos("Emociones y Salud", "Lunes 11:30 hs", "Mariela Mases", "Maipú 1065. SALON NORTE")
+const economia = new Cursos("Economía Familiar", "Lunes 14:30hs", "Luciano Jara y Juan Cercos", "Sarmiento 784. PISO 1 AULA 3")
+const etimologia = new Cursos("Etimología de las palabras", "Lunes 15hs", "Darío Maiorana", "FACULTAD HUMANIDADES Y ARTES. AULA 6C")
+const cine = new Cursos("Curso de cine: grandes directores", "Lunes 16hs", "Gisela Mattioni", "AUDITORIO APUR. CORDOBA 2965")
+const fondas = new Cursos("Relevando Fondas", "Lunes 16:30hs", "Gisela galassi", "Sarmiento 784. PISO 1 AULA 1")
+const arqueo = new Cursos("Arqueología Urbana", "Lunes 18hs", "Gustavo Fernetti y Soccorso Volpe", "Sarmiento 784. PISO 1 AULA 2")
+
+//Martes
+const miradas = new Cursos("Miradas Cotidianas", "Martes 9hs", "Martín Toyé", "FAC. HUMANIDADES Y ARTES")
+const celuChendo = new Cursos("ProUAPAM Digital - Celulares Martes", "Martes 9hs", "Lucio Chendo", "Sarmiento 784. PISO 1 AULA 3")
+const bailable = new Cursos("Experiencia Bailable", "Martes 10hs", "Paula Manaker", "CASA DEL TANGO. ATURO ILLIA 1750")
+const tt = new Cursos("Proyecto TT", "Martes 15hs", "Carolina Montano", "Sarmiento 784. PISO2 AULA 5")
+const carta = new Cursos("Filosofía a la Carta", "Martes 15:30hs", "Florencia Arigone y Maira Pouey Bragos", "Sarmiento 784. PISO 1 AULA 2")
+const sexualidad = new Cursos("Taller de Salud Sexual", "Martes 16hs", "Adriana Caille y equipo.", "Sarmiento 784. PISO 2 AULA 4")
+const ludmila = new Cursos("Filosofía en Práctica", "Martes 18hs", "Ludmila Barta", "Sarmiento 784. PISO 1 AULA 3")
+const arteymoda = new Cursos("Arte y Moda", "Martes 18hs", "Sara Franch y Julia Pieroni", "Sarmiento 784. PISO 1 AULA 2")
+const ritmos = new Cursos("Ritmos Folclóricos y Latinos", "Martes 18hs", "Pablo Rodriguez", "Sarmiento 784. PISO 2 AULA 5")
+
+//Miercoles
+const acuarela = new Cursos("Acuarela", "Miércoles 10hs", "Adriana Palma", "Sarmiento 784. PISO 2 AULA 5")
+const psico = new Cursos("Psiconeuroendocrinoinmunología", "Miércoles 11hs", "Eduardo Udisio y equipo.", "Sarmiento 784. PISO 1 AULA 2")
+const magdalena = new Cursos("Eros y la sexualidad en la Grecia Antigua", "Miércoles 15:30hs", "Magdalena Aliau", "Sarmiento 784. PISO 1 AULA 2")
+const magia = new Cursos("Iniciación a la magia", "Miércoles 16hs", "Gustavo Seoane", "Sarmiento 784. PISO 2 AULA 5")
+const textil = new Cursos("Taller de creación Textil", "Miércoles 18hs", "Ivana Mingorance", "Sarmiento 784. PISO 2 AULA 5")
+const tendencias = new Cursos("Tendencias actuales de la política Internacional", "Miércoles 18hs", "Ornela Fabani y equipo", "Sarmiento 784. PISO 1 AULA 2")
+const motola = new Cursos("Literatura, mitología y religión", "Miércoles 18hs", "Adriana Palma", "Sarmiento 784. PISO 1 AULA 3")
+const informatica = new Cursos("ProUAPAM Digital - Informática", "Miércoles 15hs", "Rocio Villoria", "FAC. CS. ECONÓMICAS")
+
+//Jueves
+const rusa = new Cursos("La revolución Rusa", "Jueves 9:30hs", "Antonio Oliva", "Sarmiento 784. PISO 2 AULA 4")
+const detti = new Cursos("Dibujar y crear con la figura humana", "Jueves 10hs", "Daniel Detti", "Sarmiento 784. PISO 2 AULA 5")
+const violencias = new Cursos("Las violencias y su relación con las pulsiones de amor", "Jueves 10hs", "Fernanda Fernández", "SALON EMPLEADOS DE COMERCIO - CORRIENTES 450")
+const rossi = new Cursos("Filosofía y el problema de la libertad", "Jueves 11:30hs", "Enrique Rossi", "Sarmiento 784. PISO 2 AULA 4")
+const celuNatali = new Cursos("ProUAPAM Digital - Celulares Jueves", "Jueves 14hs", "Natalí Tomasini", "Sarmiento 784. PISO 2 AULA 4")
+const historia = new Cursos("Historia Argentina Reciente", "Jueves 14:30hs", "Juan Manuel Nuñez", "Sarmiento 784. PISO 1 AULA 2")
+const edu = new Cursos("La conflictividad social en Argentina", "Jueves 17hs", "Eduardo Taleti", "Sarmiento 784. PISO 1 AULA 2")
+const emotango = new Cursos("Emotango", "Jueves 17:30hs", "Lucio Chendo y Constanza Juárez", "Sarmiento 784. PISO 2 AULA 5")
+const contame = new Cursos("Contame una Historia", "Jueves 18:30hs", "Jose Dalonso y M. Cecilia Cerutti", "Sarmiento 784. PISO 2 AULA 4")
+
+//Viernes
+const bienestar = new Cursos("Como lograr un estado de bienestar", "Viernes 9:30hs", "Ma. Eugenia Biancardi y equipo", "Sarmiento 784. PISO 1 AULA 2")
+const familia= new Cursos("Derecho de familia", "Viernes 9:30hs", "Rodolfo Ramos", "Sarmiento 784. PISO 2 AULA 5")
+const ford = new Cursos("Viajes del cuerpo y la política", "Viernes 11hs", "Alverto Ford y Vanesa Moreira", "Sarmiento 784. PISO 2 AULA 5")
+const campra = new Cursos("Buen Vivir", "Viernes 15hs", "Luis Campra", "Sarmiento 784. PISO 1 AULA 2")
+const coro = new Cursos("Coro", "Viernes 15hs", "Cecilia Abecasis", "Sarmiento 784. PISO 2 AULA 5")
+
+//ARRAY CON cursos
+const cursos = [bioetica, desarrolloDibujo, emociones, economia, etimologia, cine, fondas, arqueo, miradas, celuChendo, bailable, tt, carta, sexualidad, ludmila, arteymoda, ritmos, acuarela, psico, magdalena, magia, textil, tendencias, motola, rusa, detti, violencias, rossi, celuNatali, historia, edu, emotango, contame ]
+
+//VARIABLES
+let mis_cursos = document.getElementById("cursos") //CURSOS
+
+//BUSCADOR DE SUCURSALES POR ZONA
+const textoBuscar = document.querySelector("#textoBuscar")
+const botonBuscar = document.querySelector("#botonBuscar")
+const resultadoBusqueda = document.querySelector("#cursos")
+
+const buscarProductos = ()=>{   
+    
+    resultadoBusqueda.innerHTML = ""
+
+    const contCursos = textoBuscar.value.toLowerCase();    
+    for (let curso of cursos){
+        let descripcion = curso.nombre.toLowerCase() && curso.docente.toLowerCase();       
+        if(descripcion.indexOf(contCursos) !== -1){
+                resultadoBusqueda.innerHTML += `                                              
+                                                <div class="card" style="width: 20rem">
+                                                  <div class="card-body">
+                                                    <h5 class="card-title">${curso.nombre}</h5>
+                                                    <h6 class="card-subtitle mb-2 text-body-secondary">Prof. ${curso.docente}</h6>
+                                                    <h6 class="card-subtitle mb-2 text-body-secondary">Día y Hora: ${curso.dia}</h6>
+                                                    <h6 class=" lugar card-subtitle mb-2 text-body-secondary">Lugar: ${curso.lugar}</h6>
+                                                  </div>
+                                                </div>
+                                                
+                                                `
+                                           
+            }  
+            let btn_ubi = document.getElementsByClassName("btn_ubi")
+            for(let compra of btn_ubi){
+            compra.addEventListener("click", ()=>{
+            Swal.fire({
+            title: 'Ubicación',         
+            html: `<b>Últimos pasos!! Haga click en el siguiente enlace para ver COMO LLEGAR: </b> ` +
+            '<a href="https://www.google.com.ar/maps/@-32.9511051,-60.7098175,15z">VER COMO LLEGAR</a>',
+            imageUrl: 'https://i.blogs.es/635f55/maps/450_1000.webp',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+          })
+      })
+}
+    }
+    if(resultadoBusqueda.innerHTML === ""){    
+    Toastify({
+        text: "El producto no se encuentra en la tienda",
+        duration: 2000,
+        gravity: "top", 
+        position: "center", 
+        stopOnFocus: false, 
+        style: {
+          background: "red",
+          color: "white"
+        },
+      }).showToast(); 
+    }
+}
+
+botonBuscar.addEventListener("click" , buscarProductos)
+//VA BUSCANDO A MEDIDA QUE VAMOS ESCRIBIENDO EN EL BUSCADOR
+textoBuscar.addEventListener("keyup", buscarProductos)
+
+//OCULTAR SUCURSALES
+let ocultar_sucursales = document.getElementById("ocultar_sucursal")
+function ocultarSucu(){
+    resultadoBusqueda.innerHTML = ""
+  }
+//--------------- BOTON PARA OCULTAR SUCURSALES
+ocultar_sucursales.onclick = ocultarSucu
